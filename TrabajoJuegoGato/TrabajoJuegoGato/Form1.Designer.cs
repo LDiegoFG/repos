427162674e2cd4dcbc816c953aebe2f00b013636
Medina <Hector.medina18@tectijuana.edu.mx>
2018-10-26 20:08:14 -0700

    Initial commit

---
+++ TrabajoJuegoGato/TrabajoJuegoGato/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace TrabajoJuegoGato
+{
+    partial class FrmGato
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1x2 = new System.Windows.Forms.PictureBox();
+            this.pb1x1 = new System.Windows.Forms.PictureBox();
+            this.pb2x1 = new System.Windows.Forms.PictureBox();
+            this.pb2x2 = new System.Windows.Forms.PictureBox();
+            this.pb3x1 = new System.Windows.Forms.PictureBox();
+            this.pb3x2 = new System.Windows.Forms.PictureBox();
+            this.pb1x3 = new System.Windows.Forms.PictureBox();
+            this.pb2x3 = new System.Windows.Forms.PictureBox();
+            this.pb3x3 = new System.Windows.Forms.PictureBox();
+            this.lblRondas = new System.Windows.Forms.Label();
+            this.lblJugador1 = new System.Windows.Forms.Label();
+            this.lblJugador2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1x2
+            // 
+            this.pb1x2.Location = new System.Drawing.Point(150, 64);
+            this.pb1x2.Name = "pb1x2";
+            this.pb1x2.Size = new System.Drawing.Size(88, 84);
+            this.pb1x2.TabIndex = 0;
+            this.pb1x2.TabStop = false;
+            // 
+            // pb1x1
+            // 
+            this.pb1x1.Location = new System.Drawing.Point(56, 64);
+            this.pb1x1.Name = "pb1x1";
+            this.pb1x1.Size = new System.Drawing.Size(88, 84);
+            this.pb1x1.TabIndex = 0;
+            this.pb1x1.TabStop = false;
+            // 
+            // pb2x1
+            // 
+            this.pb2x1.Location = new System.Drawing.Point(56, 154);
+            this.pb2x1.Name = "pb2x1";
+            this.pb2x1.Size = new System.Drawing.Size(88, 84);
+            this.pb2x1.TabIndex = 0;
+            this.pb2x1.TabStop = false;
+            // 
+            // pb2x2
+            // 
+            this.pb2x2.Location = new System.Drawing.Point(150, 154);
+            this.pb2x2.Name = "pb2x2";
+            this.pb2x2.Size = new System.Drawing.Size(88, 84);
+            this.pb2x2.TabIndex = 0;
+            this.pb2x2.TabStop = false;
+            // 
+            // pb3x1
+            // 
+            this.pb3x1.Location = new System.Drawing.Point(56, 244);
+            this.pb3x1.Name = "pb3x1";
+            this.pb3x1.Size = new System.Drawing.Size(88, 84);
+            this.pb3x1.TabIndex = 0;
+            this.pb3x1.TabStop = false;
+            // 
+            // pb3x2
+            // 
+            this.pb3x2.Location = new System.Drawing.Point(150, 244);
+            this.pb3x2.Name = "pb3x2";
+            this.pb3x2.Size = new System.Drawing.Size(88, 84);
+            this.pb3x2.TabIndex = 0;
+            this.pb3x2.TabStop = false;
+            // 
+            // pb1x3
+            // 
+            this.pb1x3.Location = new System.Drawing.Point(244, 64);
+            this.pb1x3.Name = "pb1x3";
+            this.pb1x3.Size = new System.Drawing.Size(88, 84);
+            this.pb1x3.TabIndex = 0;
+            this.pb1x3.TabStop = false;
+            // 
+            // pb2x3
+            // 
+            this.pb2x3.Location = new System.Drawing.Point(244, 154);
+            this.pb2x3.Name = "pb2x3";
+            this.pb2x3.Size = new System.Drawing.Size(88, 84);
+            this.pb2x3.TabIndex = 0;
+            this.pb2x3.TabStop = false;
+            // 
+            // pb3x3
+            // 
+            this.pb3x3.Location = new System.Drawing.Point(244, 244);
+            this.pb3x3.Name = "pb3x3";
+            this.pb3x3.Size = new System.Drawing.Size(88, 84);
+            this.pb3x3.TabIndex = 0;
+            this.pb3x3.TabStop = false;
+            // 
+            // lblRondas
+            // 
+            this.lblRondas.AutoSize = true;
+            this.lblRondas.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRondas.Location = new System.Drawing.Point(544, 64);
+            this.lblRondas.Name = "lblRondas";
+            this.lblRondas.Size = new System.Drawing.Size(68, 24);
+            this.lblRondas.TabIndex = 1;
+            this.lblRondas.Text = "Rondas";
+            // 
+            // lblJugador1
+            // 
+            this.lblJugador1.AutoSize = true;
+            this.lblJugador1.Location = new System.Drawing.Point(457, 111);
+            this.lblJugador1.Name = "lblJugador1";
+            this.lblJugador1.Size = new System.Drawing.Size(54, 13);
+            this.lblJugador1.TabIndex = 2;
+            this.lblJugador1.Text = "Jugador 1";
+            // 
+            // lblJugador2
+            // 
+            this.lblJugador2.AutoSize = true;
+            this.lblJugador2.Location = new System.Drawing.Point(650, 111);
+            this.lblJugador2.Name = "lblJugador2";
+            this.lblJugador2.Size = new System.Drawing.Size(51, 13);
+            this.lblJugador2.TabIndex = 3;
+            this.lblJugador2.Text = "Jugador2";
+            // 
+            // FrmGato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 397);
+            this.Controls.Add(this.lblJugador2);
+            this.Controls.Add(this.lblJugador1);
+            this.Controls.Add(this.lblRondas);
+            this.Controls.Add(this.pb1x1);
+            this.Controls.Add(this.pb2x1);
+            this.Controls.Add(this.pb3x3);
+            this.Controls.Add(this.pb3x2);
+            this.Controls.Add(this.pb3x1);
+            this.Controls.Add(this.pb2x3);
+            this.Controls.Add(this.pb1x3);
+            this.Controls.Add(this.pb2x2);
+            this.Controls.Add(this.pb1x2);
+            this.Name = "FrmGato";
+            this.Text = "Gato";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1x2;
+        private System.Windows.Forms.PictureBox pb1x1;
+        private System.Windows.Forms.PictureBox pb2x1;
+        private System.Windows.Forms.PictureBox pb2x2;
+        private System.Windows.Forms.PictureBox pb3x1;
+        private System.Windows.Forms.PictureBox pb3x2;
+        private System.Windows.Forms.PictureBox pb1x3;
+        private System.Windows.Forms.PictureBox pb2x3;
+        private System.Windows.Forms.PictureBox pb3x3;
+        private System.Windows.Forms.Label lblRondas;
+        private System.Windows.Forms.Label lblJugador1;
+        private System.Windows.Forms.Label lblJugador2;
+    }
+}
+
---
+++ TrabajoJuegoGato/TrabajoJuegoGato/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoJuegoGato
+{
+    public partial class FrmGato : Form
+    {
+        public FrmGato()
+        {
+            InitializeComponent();
+        }
+    }
+}
