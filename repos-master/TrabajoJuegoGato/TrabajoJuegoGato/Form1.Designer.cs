547ce7bfc56d505391fd63011fdc13fef5047f64
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2018-11-15 22:32:20 -0800

    Intento de actualización (?)
    Estoy tratando de animar el form para que soporte imagenes con transparencia y luego lograr hacer una transición de imágenes para emular un gif.

---
+++ repos-master/TrabajoJuegoGato/TrabajoJuegoGato/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace TrabajoJuegoGato
+{
+    partial class FrmGato
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGato));
+            this.pb1x2 = new System.Windows.Forms.PictureBox();
+            this.pb1x1 = new System.Windows.Forms.PictureBox();
+            this.pb2x1 = new System.Windows.Forms.PictureBox();
+            this.pb2x2 = new System.Windows.Forms.PictureBox();
+            this.pb3x1 = new System.Windows.Forms.PictureBox();
+            this.pb3x2 = new System.Windows.Forms.PictureBox();
+            this.pb1x3 = new System.Windows.Forms.PictureBox();
+            this.pb2x3 = new System.Windows.Forms.PictureBox();
+            this.pb3x3 = new System.Windows.Forms.PictureBox();
+            this.lblTurnos = new System.Windows.Forms.Label();
+            this.lblJugador1 = new System.Windows.Forms.Label();
+            this.lblJugador2 = new System.Windows.Forms.Label();
+            this.BotonSalida = new System.Windows.Forms.Button();
+            this.BotonNuevo = new System.Windows.Forms.Button();
+            this.BotonReinicio = new System.Windows.Forms.Button();
+            this.pbRalsei = new System.Windows.Forms.PictureBox();
+            this.pcSusie = new System.Windows.Forms.PictureBox();
+            this.lblContador = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRalsei)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcSusie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1x2
+            // 
+            this.pb1x2.Image = ((System.Drawing.Image)(resources.GetObject("pb1x2.Image")));
+            this.pb1x2.Location = new System.Drawing.Point(274, 66);
+            this.pb1x2.Name = "pb1x2";
+            this.pb1x2.Size = new System.Drawing.Size(88, 84);
+            this.pb1x2.TabIndex = 0;
+            this.pb1x2.TabStop = false;
+            // 
+            // pb1x1
+            // 
+            this.pb1x1.Image = ((System.Drawing.Image)(resources.GetObject("pb1x1.Image")));
+            this.pb1x1.Location = new System.Drawing.Point(180, 66);
+            this.pb1x1.Name = "pb1x1";
+            this.pb1x1.Size = new System.Drawing.Size(88, 84);
+            this.pb1x1.TabIndex = 0;
+            this.pb1x1.TabStop = false;
+            // 
+            // pb2x1
+            // 
+            this.pb2x1.Image = ((System.Drawing.Image)(resources.GetObject("pb2x1.Image")));
+            this.pb2x1.Location = new System.Drawing.Point(180, 156);
+            this.pb2x1.Name = "pb2x1";
+            this.pb2x1.Size = new System.Drawing.Size(88, 84);
+            this.pb2x1.TabIndex = 0;
+            this.pb2x1.TabStop = false;
+            // 
+            // pb2x2
+            // 
+            this.pb2x2.Image = ((System.Drawing.Image)(resources.GetObject("pb2x2.Image")));
+            this.pb2x2.Location = new System.Drawing.Point(274, 156);
+            this.pb2x2.Name = "pb2x2";
+            this.pb2x2.Size = new System.Drawing.Size(88, 84);
+            this.pb2x2.TabIndex = 0;
+            this.pb2x2.TabStop = false;
+            // 
+            // pb3x1
+            // 
+            this.pb3x1.Image = ((System.Drawing.Image)(resources.GetObject("pb3x1.Image")));
+            this.pb3x1.Location = new System.Drawing.Point(180, 246);
+            this.pb3x1.Name = "pb3x1";
+            this.pb3x1.Size = new System.Drawing.Size(88, 84);
+            this.pb3x1.TabIndex = 0;
+            this.pb3x1.TabStop = false;
+            // 
+            // pb3x2
+            // 
+            this.pb3x2.Image = ((System.Drawing.Image)(resources.GetObject("pb3x2.Image")));
+            this.pb3x2.Location = new System.Drawing.Point(274, 246);
+            this.pb3x2.Name = "pb3x2";
+            this.pb3x2.Size = new System.Drawing.Size(88, 84);
+            this.pb3x2.TabIndex = 0;
+            this.pb3x2.TabStop = false;
+            // 
+            // pb1x3
+            // 
+            this.pb1x3.Image = ((System.Drawing.Image)(resources.GetObject("pb1x3.Image")));
+            this.pb1x3.Location = new System.Drawing.Point(368, 66);
+            this.pb1x3.Name = "pb1x3";
+            this.pb1x3.Size = new System.Drawing.Size(88, 84);
+            this.pb1x3.TabIndex = 0;
+            this.pb1x3.TabStop = false;
+            // 
+            // pb2x3
+            // 
+            this.pb2x3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pb2x3.Image = ((System.Drawing.Image)(resources.GetObject("pb2x3.Image")));
+            this.pb2x3.Location = new System.Drawing.Point(368, 156);
+            this.pb2x3.Name = "pb2x3";
+            this.pb2x3.Size = new System.Drawing.Size(88, 84);
+            this.pb2x3.TabIndex = 0;
+            this.pb2x3.TabStop = false;
+            // 
+            // pb3x3
+            // 
+            this.pb3x3.Image = ((System.Drawing.Image)(resources.GetObject("pb3x3.Image")));
+            this.pb3x3.Location = new System.Drawing.Point(368, 246);
+            this.pb3x3.Name = "pb3x3";
+            this.pb3x3.Size = new System.Drawing.Size(88, 84);
+            this.pb3x3.TabIndex = 0;
+            this.pb3x3.TabStop = false;
+            // 
+            // lblTurnos
+            // 
+            this.lblTurnos.AutoSize = true;
+            this.lblTurnos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTurnos.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnos.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblTurnos.Location = new System.Drawing.Point(283, 1);
+            this.lblTurnos.Name = "lblTurnos";
+            this.lblTurnos.Size = new System.Drawing.Size(67, 24);
+            this.lblTurnos.TabIndex = 1;
+            this.lblTurnos.Text = "Turnos";
+            // 
+            // lblJugador1
+            // 
+            this.lblJugador1.AutoSize = true;
+            this.lblJugador1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblJugador1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblJugador1.Location = new System.Drawing.Point(12, 12);
+            this.lblJugador1.Name = "lblJugador1";
+            this.lblJugador1.Size = new System.Drawing.Size(54, 13);
+            this.lblJugador1.TabIndex = 2;
+            this.lblJugador1.Text = "Jugador 1";
+            // 
+            // lblJugador2
+            // 
+            this.lblJugador2.AutoSize = true;
+            this.lblJugador2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblJugador2.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblJugador2.Location = new System.Drawing.Point(555, 9);
+            this.lblJugador2.Name = "lblJugador2";
+            this.lblJugador2.Size = new System.Drawing.Size(51, 13);
+            this.lblJugador2.TabIndex = 3;
+            this.lblJugador2.Text = "Jugador2";
+            // 
+            // BotonSalida
+            // 
+            this.BotonSalida.Location = new System.Drawing.Point(456, 368);
+            this.BotonSalida.Name = "BotonSalida";
+            this.BotonSalida.Size = new System.Drawing.Size(75, 23);
+            this.BotonSalida.TabIndex = 5;
+            this.BotonSalida.Text = "Salida";
+            this.BotonSalida.UseVisualStyleBackColor = true;
+            this.BotonSalida.Click += new System.EventHandler(this.BotonSalida_Click);
+            // 
+            // BotonNuevo
+            // 
+            this.BotonNuevo.BackColor = System.Drawing.Color.Transparent;
+            this.BotonNuevo.Location = new System.Drawing.Point(129, 368);
+            this.BotonNuevo.Name = "BotonNuevo";
+            this.BotonNuevo.Size = new System.Drawing.Size(79, 23);
+            this.BotonNuevo.TabIndex = 6;
+            this.BotonNuevo.Text = "Nuevo Juego";
+            this.BotonNuevo.UseVisualStyleBackColor = false;
+            this.BotonNuevo.Click += new System.EventHandler(this.BotonNuevo_Click);
+            // 
+            // BotonReinicio
+            // 
+            this.BotonReinicio.Location = new System.Drawing.Point(301, 368);
+            this.BotonReinicio.Name = "BotonReinicio";
+            this.BotonReinicio.Size = new System.Drawing.Size(75, 23);
+            this.BotonReinicio.TabIndex = 7;
+            this.BotonReinicio.Text = "Reinicio";
+            this.BotonReinicio.UseVisualStyleBackColor = true;
+            this.BotonReinicio.Click += new System.EventHandler(this.BotonReinicio_Click);
+            // 
+            // pbRalsei
+            // 
+            this.pbRalsei.BackColor = System.Drawing.Color.Gray;
+            this.pbRalsei.Location = new System.Drawing.Point(25, 118);
+            this.pbRalsei.Name = "pbRalsei";
+            this.pbRalsei.Size = new System.Drawing.Size(91, 143);
+            this.pbRalsei.TabIndex = 8;
+            this.pbRalsei.TabStop = false;
+            // 
+            // pcSusie
+            // 
+            this.pcSusie.BackColor = System.Drawing.Color.Gray;
+            this.pcSusie.Location = new System.Drawing.Point(515, 130);
+            this.pcSusie.Name = "pcSusie";
+            this.pcSusie.Size = new System.Drawing.Size(91, 143);
+            this.pcSusie.TabIndex = 9;
+            this.pcSusie.TabStop = false;
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblContador.Font = new System.Drawing.Font("Sitka Small", 12F);
+            this.lblContador.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblContador.Location = new System.Drawing.Point(308, 25);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(21, 24);
+            this.lblContador.TabIndex = 10;
+            this.lblContador.Text = "0";
+            // 
+            // FrmGato
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(646, 398);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.pcSusie);
+            this.Controls.Add(this.pbRalsei);
+            this.Controls.Add(this.BotonReinicio);
+            this.Controls.Add(this.BotonNuevo);
+            this.Controls.Add(this.BotonSalida);
+            this.Controls.Add(this.lblJugador2);
+            this.Controls.Add(this.lblJugador1);
+            this.Controls.Add(this.lblTurnos);
+            this.Controls.Add(this.pb1x1);
+            this.Controls.Add(this.pb2x1);
+            this.Controls.Add(this.pb3x3);
+            this.Controls.Add(this.pb3x2);
+            this.Controls.Add(this.pb3x1);
+            this.Controls.Add(this.pb2x3);
+            this.Controls.Add(this.pb1x3);
+            this.Controls.Add(this.pb2x2);
+            this.Controls.Add(this.pb1x2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmGato";
+            this.Text = "Gato";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRalsei)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcSusie)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTurnos;
+        private System.Windows.Forms.Label lblJugador1;
+        private System.Windows.Forms.Label lblJugador2;
+        private System.Windows.Forms.Button BotonSalida;
+        public System.Windows.Forms.PictureBox pb1x2;
+        public System.Windows.Forms.PictureBox pb1x1;
+        public System.Windows.Forms.PictureBox pb2x1;
+        public System.Windows.Forms.PictureBox pb2x2;
+        public System.Windows.Forms.PictureBox pb3x1;
+        public System.Windows.Forms.PictureBox pb3x2;
+        public System.Windows.Forms.PictureBox pb1x3;
+        public System.Windows.Forms.PictureBox pb2x3;
+        public System.Windows.Forms.PictureBox pb3x3;
+        private System.Windows.Forms.Button BotonNuevo;
+        private System.Windows.Forms.Button BotonReinicio;
+        private System.Windows.Forms.PictureBox pbRalsei;
+        private System.Windows.Forms.PictureBox pcSusie;
+        private System.Windows.Forms.Label lblContador;
+    }
+}
+
---
+++ repos-master/TrabajoJuegoGato/TrabajoJuegoGato/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoJuegoGato
+{
+    public partial class FrmGato : Form
+    {
+        public FrmGato()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonReinicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonSalida_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
